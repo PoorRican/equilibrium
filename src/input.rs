4ffb1877423126605f790ffc6f83dae651e4d113
Josue Figueroa <poor.rican@pm.me>
2023-12-31 15:46:18 -0500

    Add tests for `Input`

--- src/input.rs
+++ src/input.rs
@@ -1,24 +1,41 @@
 pub struct Input<F>
-where F: FnOnce() -> String {
+where F: Fn() -> String {
     callback: F,
-    state: String,
+    state: Option<String>,
 }
 
 impl<F> Input<F>
-where F: FnOnce() -> String {
+where F: Fn() -> String {
     pub fn new(callback: F) -> Input<F> {
         Input {
             callback,
-            state: String::new(),
+            state: None,
         }
     }
 
     pub fn read(&mut self) -> String {
-        self.state = (self.callback)();
-        self.state.clone()
+        let state = (self.callback)();
+        self.state = Some(state.clone());
+        state
     }
 
-    pub fn get_state(&self) -> &String {
+    pub fn get_state(&self) -> &Option<String> {
         &self.state
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_new() {
+        let mut input = super::Input::new(|| String::from("test"));
+
+        assert_eq!(input.get_state(), &None);
+
+        // Read the input
+        let state = input.read();
+        assert_eq!(state, String::from("test"));
+        assert_eq!(input.get_state(), &Some(String::from("test")));
     }
 }
