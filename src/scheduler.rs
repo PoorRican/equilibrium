0268d890fd4a2004b7f0614a81d82919d9c69394
Josue Figueroa <poor.rican@pm.me>
2023-12-31 13:59:38 -0500

    Add documentation to scheduler

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -2,7 +2,7 @@ use chrono::{DateTime, Utc};
 use crate::types::action::Action;
 use crate::types::event::Event;
 
-/// A way to manage future and past IO events
+/// A way to manage future and past [`Event`]s
 ///
 /// The purpose of this struct is to manage when IO events should be executed. "Scheduling" of
 /// events should be handled outside of this struct. This struct should only be used to determine
@@ -20,25 +20,43 @@ impl Scheduler {
         }
     }
 
+    /// Returns true if there are any future events
     pub fn has_future_events(&self) -> bool {
         !self.future_events.is_empty()
     }
 
+    /// Schedule an `On` event for the specified time
     pub fn schedule_on(&mut self, timestamp: DateTime<Utc>) {
         let event = Event::new(Action::On, timestamp);
         self.future_events.push(event);
     }
 
+    /// Schedule an `Off` event for the specified time
     pub fn schedule_off(&mut self, timestamp: DateTime<Utc>) {
         let event = Event::new(Action::Off, timestamp);
         self.future_events.push(event);
     }
 
+    /// Schedule a `Read` event for the specified time
     pub fn schedule_read(&mut self, timestamp: DateTime<Utc>) {
         let event = Event::new(Action::Read, timestamp);
         self.future_events.push(event);
     }
 
+    /// Attempt to execute any events that should be executed at the specified time
+    ///
+    /// Any event that should be executed at the specified time will be removed from the list of
+    /// future events and added to the list of past events. The action associated with the event
+    /// will be returned.
+    ///
+    /// If no events should be executed at the specified time, `None` will be returned.
+    ///
+    /// # Arguments
+    /// * `time` - The time to check for events that should be executed
+    ///
+    /// # Returns
+    /// * `Some(Action)` - The action associated with the event that should be executed
+    /// * `None` - No events should be executed at the specified time
     pub fn attempt_execution(&mut self, time: DateTime<Utc>) -> Option<Action> {
         if let Some(index) = self.future_events.iter().position(|e| e.should_execute(time)) {
             let event = self.future_events.remove(index);
