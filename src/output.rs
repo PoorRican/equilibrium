d68366593cd50c23d29e7a248ed010c179ed2608
Josue Figueroa <poor.rican@pm.me>
2023-12-31 16:01:05 -0500

    Add tests for `Output`

--- src/output.rs
+++ src/output.rs
@@ -1,7 +1,7 @@
 pub struct Output<F>
 where F: FnMut(bool) {
     callback: F,
-    state: bool,
+    state: Option<bool>,
 }
 
 impl<F> Output<F>
@@ -9,21 +9,68 @@ where F: FnMut(bool) {
     pub fn new(callback: F) -> Output<F> {
         Output {
             callback,
-            state: false,
+            state: None,
         }
     }
 
     pub fn activate(&mut self) {
-        self.state = true;
+        self.state = Some(true);
         (self.callback)(true);
     }
 
     pub fn deactivate(&mut self) {
-        self.state = false;
+        self.state = Some(false);
         (self.callback)(false);
     }
 
-    pub fn get_state(&self) -> bool {
+    pub fn get_state(&self) -> Option<bool> {
         self.state
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use std::sync::{Arc, Mutex};
+
+    #[test]
+    fn test_new() {
+        let mut output = super::Output::new(|_| {});
+
+        assert_eq!(output.get_state(), None);
+    }
+
+    #[test]
+    fn test_activate() {
+        let external_state = Arc::new(Mutex::new(false));
+        let mut output = super::Output::new(|state| {
+            let mut external_state = external_state.lock().unwrap();
+            *external_state = state;
+        });
+
+        // check default state
+        assert_eq!(output.get_state(), None);
+
+        output.activate();
+        assert_eq!(output.get_state().unwrap(), true);
+        assert_eq!(external_state.lock().unwrap().clone(), true);
+    }
+
+    #[test]
+    fn test_deactivate() {
+
+        let external_state = Arc::new(Mutex::new(true));
+        let mut output = super::Output::new(|state| {
+            let mut external_state = external_state.lock().unwrap();
+            *external_state = state;
+        });
+
+        // check default state
+        assert_eq!(output.get_state(), None);
+
+        output.deactivate();
+
+        assert_eq!(external_state.lock().unwrap().clone(), false);
+        assert_eq!(output.get_state().unwrap(), false);
     }
 }
