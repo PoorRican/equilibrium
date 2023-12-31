3a19ea3171fa0a6a1fae462a07f47af5dc38fc84
Josue Figueroa <poor.rican@pm.me>
2023-12-31 15:48:14 -0500

    Add tests for `Input`

--- src/input.rs
+++ src/input.rs
@@ -27,8 +27,18 @@ where F: Fn() -> String {
 
 #[cfg(test)]
 mod tests {
+    use std::collections::VecDeque;
+    use std::sync::{Arc, Mutex};
+
     #[test]
     fn test_new() {
+        let input = super::Input::new(|| String::from("test"));
+
+        assert_eq!(input.get_state(), &None);
+    }
+
+    #[test]
+    fn test_read() {
         let mut input = super::Input::new(|| String::from("test"));
 
         assert_eq!(input.get_state(), &None);
@@ -37,5 +47,33 @@ mod tests {
         let state = input.read();
         assert_eq!(state, String::from("test"));
         assert_eq!(input.get_state(), &Some(String::from("test")));
+    }
+
+    /// An example that shows how to get a dynamic input
+    #[test]
+    fn test_read_twice() {
+        let state_sequence =
+            Arc::new(
+                Mutex::new(
+                    VecDeque::from([
+                        "test1".to_string(),
+                        "test2".to_string()
+            ])));
+        let mut input = super::Input::new(|| {
+            let mut state_sequence = state_sequence.lock().unwrap();
+            state_sequence.pop_front().unwrap()
+        });
+
+        assert_eq!(input.get_state(), &None);
+
+        // Read the input
+        let state = input.read();
+        assert_eq!(state, String::from("test1"));
+        assert_eq!(input.get_state(), &Some(String::from("test1")));
+
+        // Read the input again
+        let state = input.read();
+        assert_eq!(state, String::from("test2"));
+        assert_eq!(input.get_state(), &Some(String::from("test2")));
     }
 }
